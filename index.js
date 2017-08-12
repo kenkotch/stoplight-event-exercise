1dac768b5a3aee738fc4352816bafe7a70407ffe
Ken Kotch <hello@ken.uno>
2017-08-11 19:55:08 -0600

    toggle buttons working, part 1 complete

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-(function() {
-  'use strict';
-
-  //eventListener
-  var listen = document.getElementById('stopButton')
-  listen.addEventListener('click', function(){
-    console.log('stop');
-    
-  })
+document.addEventListener('DOMContentLoaded', () => { //waits for page to load
 
 
+  (function() {
+    'use strict';
 
-})();
+    //constants
+    //buttons
+    const stopButton = document.getElementById('stopButton')
+    const slowButton = document.getElementById('slowButton')
+    const goButton = document.getElementById('goButton')
+    //light bulbs
+    const stopLight = document.getElementById('stopLight')
+    const slowLight = document.getElementById('slowLight')
+    const goLight = document.getElementById('goLight')
+
+    //red
+    stopButton.addEventListener('click', () => {
+      stopLight.classList.toggle('stop')
+    })
+
+    //yellow
+    slowButton.addEventListener('click', () => {
+      slowLight.classList.toggle('slow')
+    })
+
+    //green
+    goButton.addEventListener('click', () => {
+      goLight.classList.toggle('go')
+    })
+
+  })()
+})
